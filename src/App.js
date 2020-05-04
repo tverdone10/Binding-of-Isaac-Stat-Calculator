c135bd0ecd5411a492fe09406a70fb7caaf8be9e
tverdone10 <tverdone10@gmail.com>
2020-05-03 23:32:55 -0700

    functioning test home page in place. all stats generated the way the game does. isaac is a placeholder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import isaac from './images/isaachead.png';
 import './App.css';
+import TestHome from './components/testhome'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={isaac} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <TestHome />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
